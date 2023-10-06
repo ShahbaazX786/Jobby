0efa340c4703b9bfa026d63b1a9c80ccb21f9006
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2023-10-06 16:50:16 +0530

    CL-0.0.3.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
-import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Welcome, Nearbyjobs, Popularjobs, ScreenHeaderBtn } from "../components";
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Bismillah</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            {/* <Text>Bismillah</Text> */}
+            <Stack.Screen 
+            options={{ 
+                headerStyle: { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: () => (<ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />),
+                headerRight: () => (<ScreenHeaderBtn iconUrl={images.profile} dimensions="100%" />),
+                headerTitle:'' }} />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex:1, padding:SIZES.medium}}>
+                <Welcome />
+                <Nearbyjobs />
+                <Popularjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
